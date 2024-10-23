7c82106daad21393cf760f0ce6ac892c9b78af28
Chris <rolletchris1@gmail.com>
2024-10-23 14:14:03 -0700

    remove user ID from the cookie when account is deleted

--- frontend/src/components/pages/Account.js
+++ frontend/src/components/pages/Account.js
@@ -56,6 +56,7 @@ export function Account() {
             if (response.ok) {
                 Notiflix.Notify.success("Account Deleted successfully!");
                 Cookies.remove('token');
+                Cookies.remove('userId');
                 setTimeout(() => {
                     window.location.href = "/";
                 }, 1000);
