27e5856320b396ad95ec0510c3988c6903f37364
Chris <rolletchris1@gmail.com>
2024-10-22 18:23:25 -0700

    Added Encryption to username

--- backend/internal/handlers/registration.go
+++ backend/internal/handlers/registration.go
@@ -1,14 +1,16 @@
 package handlers
 
 import (
-	"github.com/gofiber/fiber/v2"
+	"encoding/base64"
+	"os"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/sandbox-science/online-learning-platform/configs/database"
 	"github.com/sandbox-science/online-learning-platform/internal/entity"
 	"github.com/sandbox-science/online-learning-platform/internal/utils"
 )
 
-// Register creates a new user and add it to the database.
+// Register creates a new user and adds it to the database.
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
 
@@ -21,8 +23,23 @@ func Register(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": "Passwords do not match"})
 	}
 
+	// Generate encryption key
+	var key [32]byte
+	keyString := os.Getenv("CRYPTO_KEY")
+	decodedKey, err := base64.StdEncoding.DecodeString(keyString)
+	if err != nil || len(decodedKey) != 32 {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "ENCRYPTION_KEY must be a valid Base64-encoded string of 32 bytes"})
+	}
+	copy(key[:], decodedKey) // Copy the decoded key to the key variable
+
+	encryptedUsername, err := utils.Encrypt(data["username"], key)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Error encrypting username"})
+	}
+
+	// Create user account
 	user := entity.Account{
-		Username: data["username"],
+		Username: encryptedUsername,
 		Email:    data["email"],
 		Password: data["password"],
 	}
