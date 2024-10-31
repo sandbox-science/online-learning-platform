10cc28e2e8fb1a583c69cdf8db6cd5963b2d4b30
zachspang <spangzach@gmail.com>
2024-10-30 19:07:52 -0700

    Add loop to display variable number of courses

--- frontend/src/components/pages/Courses.js
+++ frontend/src/components/pages/Courses.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
 export function Courses() {
+
+    //Push all courses into courseList
+    var courseList = []
+    for(var i = 1; i < 10; i++){
+        courseList.push(
+            <a href={`/courses/${i}`}>
+                <div className="bg-gray-100 p-4 rounded shadow" >
+                    <h3 className="text-xl font-semibold" >Course Title {i}</h3>
+                    <p className="mt-2">Description of Course {i}</p>
+                </div>
+            </a>
+        )
+    }
+
     return (
-        // This is a placeholder for the dashboard.
-        // This will be removed/changed when an actual implementation will be made.
         <div className="p-6">
-            <p className="text-2xl font-bold mb-4 text-center">This is a dummy dashboard!</p>
             <h1 className="text-2xl font-bold mb-4">Courses</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                <div className="bg-gray-100 p-4 rounded shadow">
-                    <h3 className="text-xl font-semibold">Course Title 1</h3>
-                    <p className="mt-2">Description of Course 1</p>
-                </div>
-                <div className="bg-gray-100 p-4 rounded shadow">
-                    <h3 className="text-xl font-semibold">Course Title 2</h3>
-                    <p className="mt-2">Description of Course 2</p>
-                </div>
-                <div className="bg-gray-100 p-4 rounded shadow">
-                    <h3 className="text-xl font-semibold">Course Title 3</h3>
-                    <p className="mt-2">Description of Course 3</p>
-                </div>
+                {courseList}
             </div>
         </div>
     );
