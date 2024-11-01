5d7aefad3923073e04cf0117ef882fa106c2b2c5
ssume1 <sharminsume99@gmail.com>
2024-11-01 03:10:19 -0700

    Fixed update_handlers.go

--- update_handlers.go
+++ update_handlers.go
@@ -13,20 +13,20 @@ import (
 // UpdateUsername updates a user's username
 func UpdateUsername(c *fiber.Ctx) error {
     var data map[string]string
-
     if err := c.BodyParser(&data); err != nil {
         return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": "Invalid input"})
     }
 
     userID := data["user_id"]
-    username := data["username"]
 
     var user entity.Account
     if err := database.DB.First(&user, userID).Error; err != nil {
         return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"message": "User not found"})
     }
 
-    user.Username = username
+    // Directly assign the username
+    user.Username = data["username"]
+
     if err := database.DB.Save(&user).Error; err != nil {
         return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Couldn't update username"})
     }
@@ -37,22 +37,21 @@ func UpdateUsername(c *fiber.Ctx) error {
 // UpdateEmail updates a user's email
 func UpdateEmail(c *fiber.Ctx) error {
     var data map[string]string
-
     if err := c.BodyParser(&data); err != nil {
         return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": "Invalid input"})
     }
 
     userID := data["user_id"]
-    email := data["email"]
-    confirmEmail := data["confirm_email"]
-
-    if email != confirmEmail {
-        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": "Emails do not match"})
-    }
 
     var user entity.Account
     if err := database.DB.First(&user, userID).Error; err != nil {
         return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"message": "User not found"})
+    }
+
+    email := data["email"]
+    confirmEmail := data["confirm_email"]
+    if email != confirmEmail {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": "Emails do not match"})
     }
 
     user.Email = email
@@ -63,28 +62,9 @@ func UpdateEmail(c *fiber.Ctx) error {
     return c.JSON(fiber.Map{"message": "Email updated successfully"})
 }
 
-// RevokeToken invalidates the token by setting its expiration time to the past
-func RevokeToken(tokenString string) error {
-    secretKey := []byte(os.Getenv("JWT_SECRET"))
-
-    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-        return secretKey, nil
-    })
-
-    if err != nil || !token.Valid {
-        return err
-    }
-
-    claims := token.Claims.(jwt.MapClaims)
-    claims["exp"] = time.Now().Add(-time.Hour).Unix() // Expire the token an hour ago
-
-    return nil
-}
-
 // UpdatePassword updates a user's password and logs them out
 func UpdatePassword(c *fiber.Ctx) error {
     var data map[string]string
-
     if err := c.BodyParser(&data); err != nil {
         return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": "Invalid input"})
     }
@@ -92,7 +72,7 @@ func UpdatePassword(c *fiber.Ctx) error {
     userID := data["user_id"]
     password := data["password"]
     confirmPassword := data["confirm_password"]
-    tokenString := c.Get("Authorization")
+    tokenString := data["token"]
 
     if password != confirmPassword {
         return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": "Passwords do not match"})
@@ -103,28 +83,21 @@ func UpdatePassword(c *fiber.Ctx) error {
         return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"message": "User not found"})
     }
 
-    hashedPassword, err := utils.HashPassword(password)
-    if err != nil {
+    user.Password = password
+    if err := utils.HashPassword(&user); err != nil {
         return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Couldn't hash password"})
     }
 
-    user.Password = hashedPassword
+    if tokenString == "" {
+        return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "Token is missing"})
+    }
+
+    if err := utils.RevokeToken(tokenString); err != nil {
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Couldn't revoke token"})
+    }
+
     if err := database.DB.Save(&user).Error; err != nil {
         return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Couldn't update password"})
-    }
-
-    // Validate and revoke the token after password change
-    if tokenString == "" {
-        return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "Authorization header is missing"})
-    }
-
-    // If the token is in the format "Bearer <token>", remove "Bearer "
-    if len(tokenString) > 7 && tokenString[:7] == "Bearer " {
-        tokenString = tokenString[7:]
-    }
-
-    if err := RevokeToken(tokenString); err != nil {
-        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "Couldn't revoke token"})
     }
 
     return c.JSON(fiber.Map{"message": "Password updated and user logged out successfully"})
