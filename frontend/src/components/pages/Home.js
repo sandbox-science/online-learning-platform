d422635bd0e6f1d985e79e9e924ae8840c1ef24e
Chris Dedman-Rollet <61106361+chrisdedman@users.noreply.github.com>
2024-11-02 08:56:26 -0700

    [Update] Removed learn about button and move link to MISSION

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -12,23 +12,26 @@ export function Home({ ApiStatus }) {
                     and manage enrollment, while students can easily access lessons and monitor their achievements. With a sleek, modern design
                     and a focus on simplicity, the platform supports personalized learning experiences and fosters engagement through intuitive
                     features like course creation, content delivery, and progress tracking.</p>
-                <a href='https://github.com/sandbox-science/online-learning-platform/blob/main/README.md' target="_blankc" rel="noreferrer">
-                    <button className="mt-2 bg-red-500 text-white font-bold py-2 px-4 rounded-md hover:bg-red-600 transition-colors duration-300">Learn More</button>
-                </a>
             </section>
 
             <section className="features">
                 <div className="feature-box">
-                    <h3>MISSION</h3>
-                    <p>Learn more about our mission and vision.</p>
+                    <a href='https://github.com/sandbox-science/online-learning-platform/blob/main/README.md' target="_blankc" rel="noreferrer">
+                        <h3><b>MISSION</b></h3>
+                        <p>Learn more about our mission and vision.</p>
+                    </a>
                 </div>
                 <div className="feature-box">
-                    <h3>COURSES</h3>
-                    <p>Browse through a variety of courses tailored to your interests.</p>
+                    <a href='#' target="_blankc" rel="noreferrer">
+                        <h3><b>COURSES</b></h3>
+                        <p>Browse through a variety of courses tailored to your interests.</p>
+                    </a>
                 </div>
                 <div className="feature-box">
-                    <h3>CONTACT</h3>
-                    <p>Reach out to us for support and inquiries.</p>
+                    <a href='#' target="_blankc" rel="noreferrer">
+                        <h3><b>CONTACT</b></h3>
+                        <p>Reach out to us for support and inquiries.</p>
+                    </a>
                 </div>
             </section>
 
@@ -36,8 +39,7 @@ export function Home({ ApiStatus }) {
                 <h2>Popular Courses</h2>
                 <div className="course-list">
                     <div className="course-box">
-                        <img src="images/graphic-design.jpg" alt="Graphic Design Course" />
-                        <h3>Placeholder</h3>
+                        <h3><b>Placeholder</b></h3>
                         <p>Nothing to show, yet</p>
                     </div>
                 </div>
