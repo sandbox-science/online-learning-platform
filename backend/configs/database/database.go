de0cdaf870b8a7946cc6a847cf06962336e71988
zachspang <spangzach@gmail.com>
2024-11-01 20:20:42 -0700

    Add course and tag schema

--- backend/configs/database/database.go
+++ backend/configs/database/database.go
@@ -34,6 +34,16 @@ func DatabaseMigration(DB *gorm.DB) (*gorm.DB, error) {
 		return nil, err
 	}
 
+	err = DB.AutoMigrate(&entity.Course{})
+	if err != nil {
+		return nil, err
+	}
+
+	err = DB.AutoMigrate(&entity.Tag{})
+	if err != nil {
+		return nil, err
+	}
+
 	fmt.Println("Migrated database!")
 
 	return DB, nil
---
+++ backend/internal/entity/course.go
@@ -0,0 +1,12 @@
+package entity
+
+import "gorm.io/gorm"
+
+type Course struct {
+	gorm.Model
+	Title string `gorm:"size:255;not null;" json:"title"`
+	Description string `gorm:"size:255;not null;" json:"description "`
+	CreatorID int `gorm:"foreignKey"`
+	Students []*Account `gorm:"many2many:enrollment;"`
+	Tags []*Tag `gorm:"many2many:course_tag;"`
+}
