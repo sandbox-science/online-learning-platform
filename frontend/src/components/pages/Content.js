9ae0b0bbdc586765f8de286aa670e8a659c3c037
zachspang <spangzach@gmail.com>
2024-11-24 14:54:46 -0800

    Remove extra whitespace

--- frontend/src/components/pages/Content.js
+++ frontend/src/components/pages/Content.js
@@ -148,7 +148,6 @@ export function Content() {
                 fileUploadHandler={handleContentUpload}
                 deleteFileHandler={contentInfo.path !== "" ? handleDeleteFile(contentID) : null}
             />
-
         );
     }
 
