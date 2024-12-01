cdee683217a6544cf973b2cb58bc00df9e20c2fe
Chris <rolletchris1@gmail.com>
2024-12-01 14:05:35 -0800

    increase body length

--- backend/internal/entity/content.go
+++ backend/internal/entity/content.go
@@ -6,7 +6,7 @@ type Content struct {
 	gorm.Model
 	Title    string `gorm:"size:255;not null;" json:"title"`
 	Path     string `gorm:"size:255;not null;" json:"path"`
-	Body     string `gorm:"size:2000;" json:"body"`
+	Body     string `gorm:"size:5000;" json:"body"`
 	Type     string `gorm:"size:255;not null;" json:"type"`
 	ModuleID uint
 	Module   Module `gorm:"foreignKey:ModuleID;references:ID" json:"-"`
